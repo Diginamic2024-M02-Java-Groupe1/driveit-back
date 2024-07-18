523e0c6856a53cf5007606ba2040f7eb8ca9b3aa
AyoubBenziza <ayoub.cbenziza@gmail.com>
2024-07-18 17:44:26 +0200

    add: OpenAPI Docs

---
+++ src/main/java/com/driveit/driveit/config/AppConfig.java
@@ -0,0 +1,20 @@
+package com.driveit.driveit.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AppConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("DriveIt API")
+                        .version("1.0")
+                        .description("DriveIt API Documentation")
+                );
+    }
+}
