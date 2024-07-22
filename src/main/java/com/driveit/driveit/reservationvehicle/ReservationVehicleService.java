8060817b64655cffdc91eb8e38a594b5c4d794d3
PicToWeb <picronantoine34@gmail.com>
2024-07-22 17:23:50 +0200

    Modification de la réservation d'un véhicule de service

--- src/main/java/com/driveit/driveit/reservationvehicle/ReservationVehicleService.java
+++ src/main/java/com/driveit/driveit/reservationvehicle/ReservationVehicleService.java
@@ -40,7 +40,7 @@ public class ReservationVehicleService {
      * Méthode faisant appel à la base de donnée pour vérifier la disponibilité d'un véhicule de service
      *
      * @param vehicleId identifiant du véhicule
-     * @param from date et heure de début de location
+     * @param from      date et heure de début de location
      * @return oui ou non en fonction de la disponiblité
      */
     public boolean isAvailableBetweenDateTimes(int vehicleId, LocalDateTime from) {
@@ -57,8 +57,8 @@ public class ReservationVehicleService {
         List<VehicleDto> vehicleDtoList = new ArrayList<>();
         List<Vehicle> vehicles = vehicleRepository.findAllAvailableVehicles();
         for (Vehicle v : vehicles) {
-            if(isAvailableBetweenDateTimes(v.getId(),
-                    Converter.stringToLocalDateTime(reserveVehicle.dateStart(),reserveVehicle.timeStart()))) {
+            if (isAvailableBetweenDateTimes(v.getId(),
+                    Converter.stringToLocalDateTime(reserveVehicle.dateStart(), reserveVehicle.timeStart()))) {
                 vehicleDtoList.add(Mapper.vehicleToDto(v));
             }
         }
@@ -68,21 +68,23 @@ public class ReservationVehicleService {
     /**
      * Méthode utiliser pour réserver un véhicule de service entre une date et heure de début et une date et heure de fin
      *
-     * @param userId identifiant de l'utilisateur
+     * @param userId         identifiant de l'utilisateur
      * @param reserveVehicle dto des données utiles pour enregistrer la location d'un véhicule de service
      * @return une chaine de caractère qui indique si la réservation a été effectuée ou non
      */
     public String reserveVehicle(int userId, ReservationVehicleDto reserveVehicle) throws appException {
-        LocalDateTime from = Converter.stringToLocalDateTime(reserveVehicle.dateStart(),reserveVehicle.timeStart());
-        if(isAvailableBetweenDateTimes(reserveVehicle.vehicleDto().getId(), from)) {
-            LocalDateTime to = Converter.stringToLocalDateTime(reserveVehicle.dateEnd(),reserveVehicle.timeEnd());
-            Collaborator collaborator = collaboratorRepository.findById(userId).get();
-            Vehicle vehicleToBook = vehicleRepository.findByRegistration(reserveVehicle.vehicleDto().getRegistration());
-            ReservationVehicle reservation = new ReservationVehicle(from,to,vehicleToBook,collaborator);
-            save(reservation);
-            return "Réservation effectuée";
+        LocalDateTime from = Converter.stringToLocalDateTime(reserveVehicle.dateStart(), reserveVehicle.timeStart());
+        if (!isAvailableBetweenDateTimes(reserveVehicle.vehicleDto().getId(), from)) {
+            throw new appException("La réservation n'a pu avoir lieu, le véhicule n'est probablement plus disponible");
         }
-        throw new appException("La réservation n'a pu avoir lieu, le véhicule n'est probablement plus disponible");
+        LocalDateTime to = Converter.stringToLocalDateTime(reserveVehicle.dateEnd(), reserveVehicle.timeEnd());
+        Collaborator collaborator = collaboratorRepository.findById(userId).get();
+        Vehicle vehicleToBook = vehicleRepository.findByRegistration(reserveVehicle.vehicleDto().getRegistration());
+        ReservationVehicle reservation = new ReservationVehicle(from, to, vehicleToBook, collaborator);
+        save(reservation);
+
+        return "Réservation effectuée";
+
 
     }
 
@@ -92,8 +94,26 @@ public class ReservationVehicleService {
     }
 
     @Transactional
+    public String updateReservationVehicle(int id, ReservationVehicleDto reservationVehicleDto) throws appException {
+        ReservationVehicle reserveFounded = reservationVehicleRepository.findById(id).orElse(null);
+        if (reserveFounded == null) {
+            throw new appException("La réservation n'est pas trouvée");
+        }
+        LocalDateTime from = Converter.stringToLocalDateTime(reservationVehicleDto.dateStart(), reservationVehicleDto.timeStart());
+        if (reserveFounded.getCollaborator().getId() == id && !isAvailableBetweenDateTimes(reservationVehicleDto.vehicleDto().getId(), from)) {
+            throw new appException("La modification n'a pu avoir lieu, le véhicule n'est probablement plus disponible");
+        }
+        reserveFounded.setStartDate(from);
+        reserveFounded.setEndDate(Converter.stringToLocalDateTime(reservationVehicleDto.dateEnd(), reservationVehicleDto.timeEnd()));
+        reservationVehicleRepository.save(reserveFounded);
+        return "La réservation a été effectuée";
+    }
+
+    @Transactional
     public void delete(int id) throws appException {
-        if (!reservationVehicleRepository.existsById(id)) { throw new appException("La réservation est introuvable");}
+        if (!reservationVehicleRepository.existsById(id)) {
+            throw new appException("La réservation est introuvable");
+        }
         reservationVehicleRepository.deleteById(id);
     }
 
